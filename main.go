ad1b2218d50f245573232d97eae17f31499f34c5
chooksy94 <chooksyanumba2594@gmail.com>
2023-11-16 20:31:31 -0500

    Dennis Toronto weather function

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
 
 const apiKey = "6dbc65c242ded0d9ca9dd9e3a7035d22" // OpenWeatherMap API key
@@ -20,10 +22,37 @@ type WeatherData struct {
 // Define a custom struct representing an object
 type WeatherObj struct {
 	Description string
-	Temp int
+	Temp        int
 }
 
-func main()  {
+func torontoWeather() WeatherObj {
+	city := "Toronto"
+	weatherURL := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apiKey)
+
+	response, err := http.Get(weatherURL)
+	if err != nil {
+		fmt.Printf("Error retriving data: %s\n", err)
+		return WeatherObj{}
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error reading response body: %s\n", err)
+		return WeatherObj{}
+	}
+
+	var weatherData WeatherData
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		fmt.Printf("Error decoding JSON: %s\n", err)
+		return WeatherObj{}
+	}
+
+	return WeatherObj{Description: weatherData.Weather[0].Description, Temp: int(weatherData.Main.Temp - 273.15)} // Convert Kelvin to Celsius
+}
+
+func main() {
 	fmt.Println("Server is listening on port 5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
